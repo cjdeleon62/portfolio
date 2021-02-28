5957013a1959240a2afd87aa927094669a0c3b71
Christopher De Leon <cjdeleon062@gmail.com>
2021-02-28 11:58:48 -0500

    add sass, color palette

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-import './Header.css';
+import './Header.scss';
 
 const Header = () => (
     <header className="Header">
