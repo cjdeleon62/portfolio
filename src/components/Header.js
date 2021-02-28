1e679ce88dc885e18e8d5922c6384ad2f02dc2ef
Christopher De Leon <cjdeleon062@gmail.com>
2021-02-28 11:40:18 -0500

    add fortawesome for react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 import './Header.css';
 
 const Header = () => (
-    <header className="Header">This is the header</header>
+    <header className="Header">
+        <div className="NavIcon">
+            <FontAwesomeIcon icon={faBars} />
+        </div>
+    </header>
 );
 
 export default Header;
